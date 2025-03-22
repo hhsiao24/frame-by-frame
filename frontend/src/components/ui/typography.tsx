97f7f579c0f52f107a724ed05f55a55d5c827755
Hannah Hsiao <hhsiao24@gmail.com>
2025-03-22 17:50:03 -0400

    Added typography and initialized react router

---
+++ frontend/src/components/ui/typography.tsx
@@ -0,0 +1,25 @@
+type Props = { children: string; className: string; }
+
+export const H1 = ({ children, className }: Props) => {
+  return (
+    <h1 className={`font-title text-xl ${className}`}>{children}</h1>
+  );
+};
+
+export const H2 = ({ children, className }: Props) => {
+  return (
+    <h1 className={`font-title text-lg ${className}`}>{children}</h1>
+  );
+};
+
+export const H3 = ({ children, className }: Props) => {
+  return (
+    <h1 className={`font-title text-base ${className}`}>{children}</h1>
+  );
+};
+
+export const P = ({ children, className }: Props) => {
+  return (
+    <h1 className={`font-text text-base ${className}`}>{children}</h1>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Home } from '@/features/home/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+);
