af1b6d9dec0306ee927002385ab5020cea4cf076
Hannah Hsiao <hhsiao24@gmail.com>
2025-03-22 18:41:28 -0400

    Created home page

--- frontend/src/components/ui/typography.tsx
+++ frontend/src/components/ui/typography.tsx
@@ -1,25 +1,25 @@
-type Props = { children: React.ReactNode; className?: string; }
+type Props = { children: string; className: string; }
 
-export const H1 = ({ children, className = '' }: Props) => {
+export const H1 = ({ children, className }: Props) => {
   return (
-    <h1 className={`font-title text-4xl ${className}`}>{children}</h1>
+    <h1 className={`font-title text-6xl ${className}`}>{children}</h1>
   );
 };
 
-export const H2 = ({ children, className = '' }: Props) => {
+export const H2 = ({ children, className }: Props) => {
   return (
-    <h2 className={`font-title text-lg ${className}`}>{children}</h2>
+    <h1 className={`font-title text-lg ${className}`}>{children}</h1>
   );
 };
 
-export const H3 = ({ children, className = '' }: Props) => {
+export const H3 = ({ children, className }: Props) => {
   return (
-    <h3 className={`font-title text-base ${className}`}>{children}</h3>
+    <h1 className={`font-title text-base ${className}`}>{children}</h1>
   );
 };
 
-export const P = ({ children, className = '' }: Props) => {
+export const P = ({ children, className }: Props) => {
   return (
-    <p className={`font-text text-xl text-neutral-600 ${className}`}>{children}</p>
+    <h1 className={`font-text text-base ${className}`}>{children}</h1>
   );
 };
